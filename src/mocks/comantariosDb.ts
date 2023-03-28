46098b6501fd4ae8dfddb92d7c6491b8339fdff4
Samuel Lima <202302913458@alunos.unifanor.edu.br>
2023-03-28 01:47:45 -0300

    Criação de mocks para simular banco
    Criação de arquivo no formato JSON para simular um banco de dados e consumo de API

---
+++ src/mocks/comantariosDb.ts
@@ -0,0 +1,6 @@
+export const comentarios =[
+    {userId: 1, pergunta: 'Pra que tipo de transformador é feito o medidor inteligente da HVEX (Omni Trafo)?', resposta: 'O Omni trafo foi desenvolvido para integrar transformadores de distribuição as redes inteligentes, o dispositivo é ideal para concessionárias, fabricantes de transformadores, ou empresas privadas que desejam ter mais controle  e informações de sua rede elétrica'},
+    {userId: 2, pergunta: 'O omni trafo pode ser utilizado como um medidor de qualidade de energia (qualimetro)?', resposta: 'Não, nesse caso o caso recomendamos a utilização de um modelo específico.'},
+    {userId: 3, pergunta: 'O omni trafo pode ser utilizado como um medidor de faturamento?', resposta: 'Não, nesse caso não recomendamos o nosso medidor, nesse caso recomendamos modelos especificados pelo módulo do PRODIST e homologados pela concessionária.'},
+    {userId: 4, pergunta: 'Ele pode ser instalado em transformadores que já estão em campo ?', resposta: 'Sim, o dispositivo pode ser instalado em equipamentos em operação, porem a aferição de temperatura do topo de óleo será apenas ambiente.'},
+]
