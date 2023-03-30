95f3d4c3a7ecd0ab60d00cdc658cf2ecd0bb1fbe
Samuelrlm <samuel_lima132@outlook.com>
2023-03-30 12:41:21 -0300

    Ajuste de nomenclatura img

--- src/pages/components/FifthCard/LeftCard/Topicos/index.tsx
+++ src/pages/components/FifthCard/LeftCard/Topicos/index.tsx
@@ -3,14 +3,14 @@ import { ContainerTopicos, Infos } from "../../../SecondCard/RightCard/TextInfo/
 export default function Topicos(){
     //Aqui vou reaproveitar o componente de topicos que está no segundo card
     const Topicos = [
-        {image: 'src/images/Icons/verificadoAzulEscuro.png', text: 'Balanço energético'},
-        {image: 'src/images/Icons/verificadoAzulEscuro.png', text: 'Auto-diagnóstico dos transformadores'},
-        {image: 'src/images/Icons/verificadoAzulEscuro.png', text: 'Medidor ideal para consumidores comerciais, industriais'},
-        {image: 'src/images/Icons/verificadoAzulEscuro.png', text: 'À prova de fraude'},
-        {image: 'src/images/Icons/verificadoAzulEscuro.png', text: 'Melhoria dos Indicadores de Continuidade'},
-        {image: 'src/images/Icons/verificadoAzulEscuro.png', text: 'Maior vida útil do ativo'},
-        {image: 'src/images/Icons/verificadoAzulEscuro.png', text: 'Medição de temperatura interna'},
-        {image: 'src/images/Icons/verificadoAzulEscuro.png', text: 'Acompanhamento em tempo real das condições do equipamento'},
+        {image: 'src/images/Icons/VerificadoAzulEscuro.png', text: 'Balanço energético'},
+        {image: 'src/images/Icons/VerificadoAzulEscuro.png', text: 'Auto-diagnóstico dos transformadores'},
+        {image: 'src/images/Icons/VerificadoAzulEscuro.png', text: 'Medidor ideal para consumidores comerciais, industriais'},
+        {image: 'src/images/Icons/VerificadoAzulEscuro.png', text: 'À prova de fraude'},
+        {image: 'src/images/Icons/VerificadoAzulEscuro.png', text: 'Melhoria dos Indicadores de Continuidade'},
+        {image: 'src/images/Icons/VerificadoAzulEscuro.png', text: 'Maior vida útil do ativo'},
+        {image: 'src/images/Icons/VerificadoAzulEscuro.png', text: 'Medição de temperatura interna'},
+        {image: 'src/images/Icons/VerificadoAzulEscuro.png', text: 'Acompanhamento em tempo real das condições do equipamento'},
 
     ]
     const largura = window.innerWidth;
